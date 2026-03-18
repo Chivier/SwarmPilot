ceecb813f498d07290b44a631f43a713cec350cb
cydia2001 <cydia2001@duck.com>
2026-03-18 13:07:16 +0000

    refactor: remove dead re-exports from _predictor_lib.py

--- swarmpilot/scheduler/clients/_predictor_lib.py
+++ swarmpilot/scheduler/clients/_predictor_lib.py
@@ -13,30 +13,18 @@ Usage:
     )
 """
 
-from swarmpilot.errors import (
-    ModelNotFoundError,
-    PredictionError,
-    PredictorError,
-    PredictorValidationError as ValidationError,
-    TrainingError,
-)
 from swarmpilot.predictor.api.cache import ModelCache
 from swarmpilot.predictor.api.core import PredictorLowLevel
 from swarmpilot.predictor.models import PlatformInfo
 from swarmpilot.predictor.predictor.expect_error import ExpectErrorPredictor
 from swarmpilot.predictor.predictor.quantile import QuantilePredictor
 from swarmpilot.predictor.preprocessor.adapters import V1PreprocessorAdapter
-from swarmpilot.predictor.preprocessor.base_preprocessor_v2 import (
-    BasePreprocessorV2,
-    FeatureContext,
-)
+
 from swarmpilot.predictor.preprocessor.chain_v2 import PreprocessorChainV2
 from swarmpilot.predictor.preprocessor.preprocessors_registry import (
     PreprocessorsRegistry,
 )
-from swarmpilot.predictor.preprocessor.registry_v2 import (
-    PreprocessorsRegistryV2,
-)
+
 from swarmpilot.predictor.storage.model_storage import ModelStorage
 from swarmpilot.predictor.utils.experiment import (
     generate_experiment_prediction,
@@ -51,23 +39,15 @@ PREDICTOR_CLASSES = {
 
 __all__ = [
     "PREDICTOR_CLASSES",
-    "BasePreprocessorV2",
     "ExpectErrorPredictor",
-    "FeatureContext",
     "ModelCache",
-    "ModelNotFoundError",
     "ModelStorage",
     "PlatformInfo",
-    "PredictionError",
-    "PredictorError",
     "PredictorLowLevel",
     "PreprocessorChainV2",
     "PreprocessorsRegistry",
-    "PreprocessorsRegistryV2",
     "QuantilePredictor",
-    "TrainingError",
     "V1PreprocessorAdapter",
-    "ValidationError",
     "generate_experiment_prediction",
     "is_experiment_mode",
 ]
